ae71a2c0a12f9bf6c048cb841bb39517e500282e
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-02 02:23:42 +0500

    main section started

---
+++ src/components/featured/featured.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './featured.css'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+        <div className="featuredItem">
+            <img src='https://q-xx.bstatic.com/xdata/images/hotel/263x210/595550862.jpeg?k=3514aa4abb76a6d19df104cb307b78b841ac0676967f24f4b860d289d55d3964&o=' alt="" className='featuredImg'/>
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src='https://r-xx.bstatic.com/xdata/images/hotel/263x210/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=' alt="" className='featuredImg'/>
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src='https://r-xx.bstatic.com/xdata/images/hotel/263x210/595551044.jpeg?k=262826efe8e21a0868105c01bf7113ed94de28492ee370f4225f00d1de0c6c44&o=' alt="" className='featuredImg'/>
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src='https://q-xx.bstatic.com/xdata/images/hotel/263x210/620168315.jpeg?k=300d8d8059c8c5426ea81f65a30a7f93af09d377d4d8570bda1bd1f0c8f0767f&o=' alt="" className='featuredImg'/>
+            <div className="featuredTitles">
+                <h1>Dublin</h1>
+                <h2>123 properties</h2>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Featured
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar.jsx'
 import Header from '../../components/header/Header.jsx'
+import Featured from '../../components/featured/featured.jsx'
+import "./home.css"
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Featured/>
+      </div>
     </div>
   )
 }
