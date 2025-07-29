3921345613bf78308449817aaca3c1f1bdb61788
Abdullah-Jawed7 <oneandonly0o0.hhh7@gmail.com>
2025-07-29 15:35:52 +0500

    slider completed

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,38 +1,106 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Header from "../../components/header/Header.jsx"
-import Navbar from "../../components/navbar/Navbar.jsx"
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
-import MailList from "../../components/mailList/MailList.jsx"
-import Footer from "../../components/footer/Footer.jsx"
-import {useState} from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "../../components/header/Header.jsx";
+import Navbar from "../../components/navbar/Navbar.jsx";
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+  faCircleXmark,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import MailList from "../../components/mailList/MailList.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+import { act, useState } from "react";
 function Hotel() {
-  const [slideNumber , setSlideNumber] = useState(0)
-  const [open , setOpen] = useState(false)
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(true);
   const photos = [
-    {src:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o="},
-    {src:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o="},
-    {src:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o="},
-    {src:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o="},
-    {src:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o="},
-    {src:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o="},
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o=",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o=",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o=",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o=",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o=",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/612966938.jpg?k=6360257ba784391efd0055d1f3595b1f6335f564a8ab758869919cef1242a1da&o=",
+    },
+  ];
 
-  ]
-
-  const handleOpen = (i)=>{
+  const handleOpen = (i) => {
     setSlideNumber(i);
-    setOpen(true)
-  }
+    setOpen(true);
+  };
+  const handleSlider = (action) => {
+    console.log(slideNumber)
+   let newSlideNumber ;
+   if (action === "r") {
+
+     newSlideNumber = slideNumber === photos.length -1 ? 0 : slideNumber +1
+  } else {
+     newSlideNumber = slideNumber === 0 ? photos.length -1 : slideNumber-1
+    
+   }
+setSlideNumber(newSlideNumber)
+
+                 // Another way
+    // if (slideNumber == photos.length - 1) {
+    //   action == "r" ? setSlideNumber(0) : setSlideNumber(slideNumber - 1);
+    // } else if (slideNumber == 0) {
+    //   action == "r"
+    //     ? setSlideNumber(slideNumber + 1)
+    //     : setSlideNumber(photos.length - 1);
+    // } else {
+    //   action == "r"
+    //     ? setSlideNumber(slideNumber + 1)
+    //     : setSlideNumber(slideNumber - 1);
+    // }
+  };
   return (
     <div>
-      <Navbar/>
-      <Header type={"list"}/>
-      <div className="hotelContainer flex justify-center mt-5">
-       {open && <div className="slider"></div>}
+      <Navbar />
+      <Header type={"list"} />
+      <div className="hotelContainer flex flex-col items-center justify-center mt-5">
+        {open && (
+          <div className="slider sticky top-0 left-0 w-screen h-screen bg-black z-30 flex items-center">
+            <FontAwesomeIcon
+              onClick={() => setOpen(false)}
+              icon={faCircleXmark}
+              className="close absolute top-5 right-5 text-3xl text-gray-300 cursor-pointer"
+            />
+            <FontAwesomeIcon
+              onClick={() => handleSlider("l")}
+              icon={faCircleArrowLeft}
+              className="m-5 text-5xl cursor-pointer text-gray-300 "
+            />
+            <div className="sliderWrapper w-full h-full flex justify-center items-center">
+              <img
+                className="sliderImg w-4/5 h-4/5 "
+                src={photos[slideNumber].src}
+                alt=""
+              />
+            </div>
+            <FontAwesomeIcon
+              onClick={() => handleSlider("r")}
+              icon={faCircleArrowRight}
+              className="m-5 text-5xl cursor-pointer text-gray-300 "
+            />
+          </div>
+        )}
         <div className="hotelWrapper w-full max-w-5xl flex flex-col justify-center gap-2.5 relative">
-          <button className="absolute top-2.5 right-0 border-none py-2.5 px-5 bg-[#0071c2] text-white font-bold rounded-sm cursor-pointer">Reserve or Book Now!</button>
+          <button className="absolute top-2.5 right-0 border-none py-2.5 px-5 bg-[#0071c2] text-white font-bold rounded-sm cursor-pointer">
+            Reserve or Book Now!
+          </button>
           <div className="hotelTitle text-2xl font-bold">Grand Hotel</div>
           <div className="hotelAddress text-xs flex items-center gap-2.5">
-            <FontAwesomeIcon icon={faLocationDot}/>
+            <FontAwesomeIcon icon={faLocationDot} />
             <span>Elton St 125 New york</span>
           </div>
           <span className="hotelDistance text-[#0071c2] font-medium ">
@@ -41,43 +109,73 @@ function Hotel() {
           <span className="hotelPriceHighLight text-[#008009] font-medium">
             Book a stay over $114 at this property and get a free airport taxi
           </span>
-          
+
           <div className="hotelImages flex flex-wrap justify-between ">
-            {photos?.map((photo , i) => (
-              <div className="hotelImgWrapper w-1/3 p-1 ">
-                <img onClick={()=> handleOpen(i)} src={photo.src} alt="" className="hotelImg w-full object-cover" />
+            {photos?.map((photo, i) => (
+              <div key={i} className="hotelImgWrapper w-1/3 p-1 ">
+                <img
+                  onClick={() => handleOpen(i)}
+                  src={photo.src}
+                  alt=""
+                  className="hotelImg w-full object-cover"
+                />
               </div>
-          ))}
+            ))}
           </div>
           <div className="hotelDetails flex justify-between gap-5 mt-5">
             <div className="hotelDetailText flex-grow-3 shrink basis-0">
-              <h1 className="hotelTitle font-bold">Stay in the heart of karachi</h1>
+              <h1 className="hotelTitle font-bold">
+                Stay in the heart of karachi
+              </h1>
               <p className="hotelDesc text-sm mt-5">
-              Harum cumque autem libero impedit iure, eius eaque enim consequuntur natus quidem incidunt dicta totam cum deleniti temporibus excepturi corrupti, aliquid inventore magni modi. Dolor quo totam exercitationem corporis cupiditate.
-              Facilis harum deserunt, illo labore optio veniam quos porro ea, id voluptates nostrum esse accusamus neque in aspernatur excepturi iusto, accusantium perspiciatis quasi pariatur. Provident, illo. Laborum numquam inventore esse!
-              Ipsum facere illo autem sit molestiae corrupti, illum harum saepe doloribus cupiditate a similique unde quo? Tempora temporibus ea minus laboriosam fuga fugiat molestias, hic consectetur aspernatur ullam perspiciatis debitis?
-              Placeat tempora voluptas ducimus optio expedita excepturi natus ullam veritatis, harum cum quasi consectetur corporis. Consequatur doloribus excepturi dolores? Voluptatibus minima, ipsa amet non a ea quam ex reprehenderit vitae.
-              Neque enim nam quam temporibus ipsum, illum mollitia labore cum? Aperiam, molestiae ex? Rem, suscipit facilis adipisci debitis quidem, doloribus tempore rerum dolorem hic temporibus nisi maiores perspiciatis provident dignissimos.
-              Blanditiis exercitationem pariatur odit officia nulla vel quisquam ipsam libero enim, deserunt nihil, alias dicta. Maiores consectetur tempora, amet necessitatibus quasi enim. Laudantium eveniet temporibus, dignissimos corrupti necessitatibus cupiditate numquam.</p>
+                Harum cumque autem libero impedit iure, eius eaque enim
+                consequuntur natus quidem incidunt dicta totam cum deleniti
+                temporibus excepturi corrupti, aliquid inventore magni modi.
+                Dolor quo totam exercitationem corporis cupiditate. Facilis
+                harum deserunt, illo labore optio veniam quos porro ea, id
+                voluptates nostrum esse accusamus neque in aspernatur excepturi
+                iusto, accusantium perspiciatis quasi pariatur. Provident, illo.
+                Laborum numquam inventore esse! Ipsum facere illo autem sit
+                molestiae corrupti, illum harum saepe doloribus cupiditate a
+                similique unde quo? Tempora temporibus ea minus laboriosam fuga
+                fugiat molestias, hic consectetur aspernatur ullam perspiciatis
+                debitis? Placeat tempora voluptas ducimus optio expedita
+                excepturi natus ullam veritatis, harum cum quasi consectetur
+                corporis. Consequatur doloribus excepturi dolores? Voluptatibus
+                minima, ipsa amet non a ea quam ex reprehenderit vitae. Neque
+                enim nam quam temporibus ipsum, illum mollitia labore cum?
+                Aperiam, molestiae ex? Rem, suscipit facilis adipisci debitis
+                quidem, doloribus tempore rerum dolorem hic temporibus nisi
+                maiores perspiciatis provident dignissimos. Blanditiis
+                exercitationem pariatur odit officia nulla vel quisquam ipsam
+                libero enim, deserunt nihil, alias dicta. Maiores consectetur
+                tempora, amet necessitatibus quasi enim. Laudantium eveniet
+                temporibus, dignissimos corrupti necessitatibus cupiditate
+                numquam.
+              </p>
             </div>
             <div className="hotelDetailPrice flex-1 bg-[#ebf3ff] p-5 flex flex-col gap-5">
-              <h1 className=" font-bold text-lg text-[#555]">Perfect for a 9-night stay!</h1>
+              <h1 className=" font-bold text-lg text-[#555]">
+                Perfect for a 9-night stay!
+              </h1>
               <span className="text-sm">
-                located in the real heart of karachi ,this property has an excellent location score of 9.8
+                located in the real heart of karachi ,this property has an
+                excellent location score of 9.8
               </span>
               <h2 className="font-light">
                 <b className="font-black">$945</b> (9 nights)
               </h2>
-              <button className="border-none py-2.5 px-5 bg-[#0071c2] text-white font-bold cursor-pointer rounded-sm">Reserve or Book Now!</button>
+              <button className="border-none py-2.5 px-5 bg-[#0071c2] text-white font-bold cursor-pointer rounded-sm">
+                Reserve or Book Now!
+              </button>
             </div>
           </div>
         </div>
       </div>
-        <MailList/>
-        <Footer/>
-   
+      <MailList />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Hotel
+export default Hotel;
